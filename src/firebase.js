01521a189b09082ae0b234b1cc855ed68f1ae5b6
Andre Bom Alves Nunes <andrebomalves@msn.com>
2019-11-19 22:06:49 -0300

    criacao do projeto

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCV-h9GKPjvfhWwsilByBoyN3O8uAWqMrw",
+  authDomain: "comentaki-andre.firebaseapp.com",
+  databaseURL: "https://comentaki-andre.firebaseio.com",
+  projectId: "comentaki-andre",
+  storageBucket: "comentaki-andre.appspot.com",
+  messagingSenderId: "218605773068",
+  appId: "1:218605773068:web:436dfa63dd296ffc956929"
+}
+
+firebase.initializeApp(firebaseConfig)
+
+export default firebase
