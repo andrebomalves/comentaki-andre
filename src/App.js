33de60f9475849e03998b1c9f9409ae5e891477b
Andre Bom Alves Nunes <andrebom@MacBook-Air-de-Andre.local>
2019-11-21 13:24:45 -0300

    hook get firebase

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@ import React, {useState, useEffect} from 'react'
 import './App.css'
 import firebase from './firebase'
 
-
-
-function App() {
-  //Hooks
+const useDataBase = endpoint => {
   const [data, setData] = useState({})
 
   useEffect(() => {
-    const ref = firebase.database().ref('test')
+    const ref = firebase.database().ref(endpoint)
     ref.on( 'value',snapshot => {
     setData(snapshot.val())
     })
@@ -17,12 +14,31 @@ function App() {
       console.log('clear')
       ref.off()
     }
-  }, [])
-  
+  }, [endpoint])
 
+  return data
+}
+
+const A = () =>{
+  const data = useDataBase('test/a')
+  return(
+    <pre>{JSON.stringify(data)}</pre>
+  )
+}
+
+const Comments = () => {
+  const data = useDataBase('test')
+  return(
+    <pre>{JSON.stringify(data)}</pre>
+  )
+}
+
+function App() {
+  //Hooks
   return (
     <div>
-      <pre>{JSON.stringify(data)}</pre>
+      <Comments />
+      <A />
     </div>
   );
 }
